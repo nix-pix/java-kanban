a7b5d7ab0392ade6f927fbe26469b8835cd78a33
nix-pix <nix-pix@yandex.ru>
2022-12-25 12:20:08 +0300

    Начал вникать в ТЗ-7

---
+++ test/ru/yandex/practicum/tasktracker/model/TaskTest.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.tasktracker.model;
+
+public class TaskTest {
+
+//    @Test
+//    public void shouldReturn2ForCalculateOneOnePlus() {
+//        int sum = calculator.calculatePolishNotation("1 1 +");
+//        Assertions.assertEquals(2, sum);
+//    }
+}
