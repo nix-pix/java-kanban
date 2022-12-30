e9971e9c11ae0c610580ba25782bc564d6f7c301
nix-pix <nix-pix@yandex.ru>
2022-12-30 13:11:04 +0300

    Написал TaskTest

--- test/ru/yandex/practicum/tasktracker/model/TaskTest.java
+++ test/ru/yandex/practicum/tasktracker/model/TaskTest.java
@@ -1,10 +1,28 @@
 package ru.yandex.practicum.tasktracker.model;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TaskTest {
+import java.time.LocalDateTime;
+
+public class TaskTest {
+
+    private static Task task;
+
+    @BeforeEach
+    public void beforeEach() {
+        task = new Task(1, TaskType.TASK, "Тестовая задача", "Тест", TaskStatus.NEW);
+    }
 
     @Test
     void getEndTime() {
+        LocalDateTime startTime = LocalDateTime.of(2022, 12, 10, 16, 0);
+        long duration = 30;
+        task.setStartTime(startTime);
+        task.setDuration(duration);
+        LocalDateTime taskEndTime = task.getEndTime();
+        LocalDateTime expectedEndTime = startTime.plusMinutes(duration);
+        Assertions.assertTrue(expectedEndTime.isEqual(taskEndTime));
     }
 }
